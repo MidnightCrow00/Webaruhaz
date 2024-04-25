701127489a0139d7c1fe0294270db639a620c620
MidnightCrow00 <orsolyaalbert1@gmail.com>
2024-04-25 11:27:51 +0200

    szerkesztve

---
+++ adat.js
@@ -0,0 +1,39 @@
+export const allatokLISTA = [
+    {
+      nev: "Bárány, Birka",
+      ar: 35000,
+      kep: 'https://hargitanepe.ro/wp-content/uploads/2016/01/IMG_1345.jpg',
+      leiras: "Idősebb és fiatalabb birkák, bárányok, jerkék és kosok vegyesen. 35ezer/db.",
+    },
+    {
+        nev: "Eladó magyartarka tehén bika borjával",
+        ar: 250000,
+        kep: 'https://th.bing.com/th/id/OIP.bZbitTXhqFNnobVi3wpGwQHaEK?rs=1&pid=ImgDetMain',
+        leiras: "Eladó 5 éves magyartarka tehén 3 hetes Bika borjaval csak egybe eladó 4 es mentes állományból. 250ezer egyben.",
+    },
+    {
+        nev: "Vietnámi keresztezett malac eladó ",
+        ar: 15000,
+        kep: 'https://aprohirdetesingyen.hu/hirdetesek/ads_new2/558/5580897/vietnami-csungohasu-malacok_11457809107.jpg',
+        leiras: "Vietnámi keresztezett malac eladó 15000 Ft/db. Amíg látod a hirdetésem keress bátran visszahívlak. Szállításban tudok segíteni ha eljössz és megnézted.",
+    },
+    {
+        nev: "Holland sportló",
+        ar: 1500000,
+        kep: 'https://aaaxmdzqmq.cloudimg.io/v7/https://ogloszenia.re-volta.pl/img/g/duze/2040122.jpg?width=1200&height=null&func=null&grey=0&blur=0&blur_faces=0&gravity=null&force_format=webp,jpeg&ci_url_encoded=1',
+        leiras: "Eladó egy extra térölelö mozgásu tisztaverü 2,5 éves kwpn kanca csikó teljes szarmazási lappal csippel rendelkezik további információ telefonon",
+    },
+    {
+        nev: "Kaliforniai húsvéti nyúl eladó",
+        ar: 3000,
+        kep: 'https://ferma.expert/wp-content/uploads/2019/02/wp20160702103315pro.webp',
+        leiras: "Eladó Kaliforniai kisnyulak több vérvonalból. Fekete, barna színből. 3ezer Ft/db",
+    },
+    {
+        nev: "Láma fiú csikó",
+        ar: 200000,
+        kep: 'https://th.bing.com/th/id/R.2ee487bd8fdca8cbc3ade0f8e17a12e2?rik=By9IozAfnKKN7g&riu=http%3a%2f%2fmiskolczoo.hu%2fsites%2fdefault%2ffiles%2fkepek%2fvezerkep%2flama1.jpg&ehk=ph5QRC35L3r%2bADe9TYe%2fqjyrGvw2AoRH3v8c3pT6NGw%3d&risl=&pid=ImgRaw&r=0',
+        leiras: "2023. júniusi születésű láma csődör csikó új gazdát keres. Szelíd és barátságos!",
+    },
+  ];
+  
---
+++ main.js
@@ -0,0 +1,7 @@
+import { allatokLISTA } from "./adat.js";
+import { megjelenit,megjelenitKetto ,tablazatLetrehoz } from "./adatkezelo.js";
+
+let txt = tablazatLetrehoz(allatokLISTA);
+megjelenit(txt);
+megjelenitKetto(txt);
+
